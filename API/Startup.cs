f09ba9fc6c0edc1dd4e47813bf07835d6540b09b
Rajender Akula <raj@Rajenders-MacBook-Pro.local>
2020-01-12 18:45:44 -0600

    Add info to startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,6 +25,8 @@ namespace API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+
+        //Info: order inside this method is not required
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(opt => {
@@ -34,6 +36,8 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+        //Info: order in this method is required
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
